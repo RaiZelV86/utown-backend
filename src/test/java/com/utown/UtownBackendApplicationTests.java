dd68421849d6d7ed7d54f3b553adc282b04dcda6
Vlad Kim <raizel@MacBook-Air-Vlad.local>
2025-12-25 18:43:29 +0900

    chore: initialize Spring Boot project with JDK 21 and Lombok

---
+++ src/test/java/com/utown/UtownBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtownBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
