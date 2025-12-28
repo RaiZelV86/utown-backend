a08285cd1e5896a83d0a50823372b61aff4cb98d
Vlad Kim <raizel@MacBook-Air-Vlad.local>
2025-12-28 18:12:08 +0900

    feat: create JwtTokenProvider with access and refresh token generation

--- src/main/java/com/utown/config/Jwtproperties.java
+++ src/main/java/com/utown/config/Jwtproperties.java
@@ -1,4 +1,14 @@
 package com.utown.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+@Data
 public class Jwtproperties {
+    private String secret;
+    private Long accessTokenExpiration;
+    private Long refreshTokenExpiration;
 }
