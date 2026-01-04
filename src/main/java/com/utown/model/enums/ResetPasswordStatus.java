b4e4a94b16351fe96263afa108e537014774fcc4
Vlad Kim <raizel@MacBook-Air-Vlad.local>
2026-01-04 15:44:19 +0900

    feat: create PasswordResetRequest entity and repository

---
+++ src/main/java/com/utown/model/enums/ResetPasswordStatus.java
@@ -0,0 +1,9 @@
+package com.utown.model.enums;
+
+public enum ResetPasswordStatus {
+    PENDING,
+    VERIFIED,
+    COMPLETED,
+    EXPIRED,
+    INVALIDATED
+}
---
+++ src/main/java/com/utown/repository/PasswordResetRequestRepository.java
@@ -0,0 +1,14 @@
+package com.utown.repository;
+
+import com.utown.model.entity.PasswordResetRequest;
+import com.utown.model.enums.ResetPasswordStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PasswordResetRequestRepository  extends JpaRepository<PasswordResetRequest,Long> {
+    Optional<PasswordResetRequest> findByUserIdAndStatus(Long userId, ResetPasswordStatus status);
+    void deleteByUserId(Long userId);
+}
